fd1812dc9a5e89f950a8eb792f3b7c723965a643
Marek <gitlab@marekurban.de>
2017-07-02 12:19:22 +0200

    Configures basic chat functionality

---
+++ client/js/app.js
@@ -0,0 +1,11 @@
+$(function () {
+  var socket = io();
+  $('form').submit(function(){
+    socket.emit('chat message', $('#message').val());
+    $('#message').val('');
+    return false;
+  });
+  socket.on('chat message', function(msg){
+    $('#messages').append($('<li>').text(msg));
+  });
+});
--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var static = __dirname + '/client/';
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+app.get('/', function(req, res) {
+  res.sendFile(static + 'index.html');
 });
 
-http.listen(3000, function(){
+app.use('/', express.static(static));
+
+io.on('connection', function(socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+    console.log('message: ' + msg);
+  });
+});
+
+http.listen(3000, function() {
   console.log('listening on *:3000');
 });
